d91270b4c9f7c575584a0edd7cebd88303604e00
regestea <regestea@outlook.com>
2026-06-25 01:11:13 +0330

    remove extera imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,14 @@
 import "./App.css";
-import {useEffect } from "react";
-import useModal from "../shared/hooks/useModal.ts";
-import Modal from "../shared/components/Modal.tsx";
-import { useThemeStore, type ThemeName } from "../shared/store/themeStore.ts";
 import { Outlet } from "react-router";
 import Nav from "../shared/components/Nav.tsx";
-import { useNavStore } from "../shared/store/navStore.ts";
 
 function App() {
-  
-
   return (
     <>
-
       <div className="main-wrapper">
         <Outlet />
       </div>
-
       <Nav />
-
-      
     </>
   );
 }
